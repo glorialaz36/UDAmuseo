04d5b3202dea4705390f6bde5ad596329ffa4b90
LucaMason <64423897+LucaMason@users.noreply.github.com>
2020-05-09 14:17:26 +0200

    aggiunta aministrazione.php aggiornamento altri

--- uda_museo/sito/php/auto_login.php
+++ uda_museo/sito/php/auto_login.php
@@ -14,7 +14,23 @@
 			if(mysqli_num_rows($query)>0){
 				$_SESSION['email']=$email;
 				$_SESSION['password']=$password;
+				$sql2="select * from AMMINISTRATORE where email='$email'";
+				$query2 = mysqli_query($mysqli,$sql2);
+				if($query2){
+					if(mysqli_num_rows($query2)>0) {
+						
+						//loggato come amministratore
+						while($riga=mysqli_fetch_array($query2)){
+						$_SESSION['amministratore']=true;
+						$_SESSION['mansione']=$riga['mansione'];
+					}
+						
+				}else{
+					//loggato come utente visitatore
+					$_SESSION['amministratore']=false;
+					$_SESSION['mansione']=0;
 				header("location: index.php"); 
+			}
 			}else{
 					"errore sconosciuto";
 				}
@@ -22,4 +38,5 @@
 		}else{
 			echo "Error: ". $sql . "<br>" .mysqli_error($mysqli);
 		}
+	}
 	?>
