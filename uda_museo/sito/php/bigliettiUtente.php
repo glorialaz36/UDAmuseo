fa845242017fe43003735c6bce11a6e90a07d98e
bastinicarlo <bastinicarlo@gmail.com>
2020-05-12 12:20:48 +0200

    Update bigliettiUtente.php
    modifica tabella biglietti acquistati, aggiunta del prezzo

--- uda_museo/sito/php/bigliettiUtente.php
+++ uda_museo/sito/php/bigliettiUtente.php
@@ -5,24 +5,36 @@
 ?>
 Biglietti acquistati:
 <?php
-	$sql="	SELECT dataVal, descCat, titolo
-			FROM biglietto, categoria, evento
-			WHERE Eve = codEve AND catBig = codCat AND vis = \"".$_SESSION['email']."\";";
+	$sql="SELECT dataVal, descCat, titolo, (tariffa-(tariffa*sconto/100)+IFNULL(pAcc, 0)) as prezzo
+		  FROM biglietto JOIN categoria ON catBig = codCat JOIN evento ON Eve = codEve LEFT JOIN(SELECT cBigl, SUM(przun) as pAcc
+																								   FROM biglacc, accessorio
+                                                                                                   WHERE cAcc = codAcc
+                                                                                                   GROUP BY cBigl) as Acc ON codBig = cBigl
+          WHERE vis = \"".$_SESSION['email']."\";";
 	$result = $mysqli -> query($sql);
+	$sql1="";
+	$result1 = $mysqli -> query($sql1);
 	//controllo esito query
 	if($result){
 		//esito positivo
-		echo "<table>";
-			echo "<tr>";
-					echo "<td>Data acquisto</td>";
-					echo "<td>Evento</td>";
-					echo "<td>Categoria</td>";
-				echo "</tr>";
+		//creazione tabella
+		echo "<table border = 1>";
+			//titoli delle colonne
 			while($row = $result -> fetch_assoc()){
+				//valori
 				echo "<tr>";
-					echo "<td>".$row['dataVal']."</td>";
-					echo "<td>".$row['titolo']."</td>";
-					echo "<td>".$row['descCat']."</td>";
+					echo "<td>";
+						echo "<table>";
+							echo "<tr>";
+								echo "<th>".$row['titolo']."</th>";
+								echo "<td>".$row['descCat']."</td>";
+								echo "<td>".$row['prezzo']." &#8364</td>";
+							echo "</tr>";
+							echo "<tr>";
+								echo "<td>".$row['dataVal']."</td>";
+							echo"</tr>";
+						echo "</table>";
+					echo "</td>";
 				echo "</tr>";
 			}
 		echo "</table>";
