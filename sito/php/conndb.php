b133adb38f3dd7d68dba1ac4b77523662d06485b
bastinicarlo <bastinicarlo@gmail.com>
2020-05-16 10:26:50 +0200

    eliminazione erbacce master

--- sito/php/conndb.php
+++
@@ -1,7 +0,0 @@
-<?php
-$mysqli = mysqli_connect("127.0.0.1","museof1","","my_museof1");
-
-if(mysqli_connect_errno($mysqli)){
-	echo "failed to connect to the database ". mysqli_connect_error();
-}
-?>
