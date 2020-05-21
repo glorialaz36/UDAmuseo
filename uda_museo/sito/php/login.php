d0230aae0daccf3ce5b578a1e5b241aba310b57e
bastinicarlo <bastinicarlo@gmail.com>
2020-05-21 16:04:12 +0200

    Modifica menu login/register

--- uda_museo/sito/php/login.php
+++ uda_museo/sito/php/login.php
@@ -50,8 +50,11 @@
 						if(!isset($_SESSION['nome'])){
 							//non è loggato
 							echo "<li class='dropdown nav-item'>";
-								echo "<a class='nav-link' href='#'>";
-									echo "<i class='fa fa-user' ></i></a>";
+                                echo "<a class='nav-link' data-toggle='dropdown'><i class='fa fa-user'></i><i class='fa fa-angle-down'></i></a>";
+								echo "<ul class='dropdown-menu' id='dropdown'>";
+									echo "<li><a class='nav-link-drop' href='sito/php/login.php'>login</a></li>";
+									echo "<li ><a class='nav-link-drop' href='sito/php/register.php'>register</a></li>";
+								echo "</ul>";
 							echo "</li>";
 						}else{
                             //è loggato
